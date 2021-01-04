650b611fff38babf3c921fdf0510e63955997ee3
Lukas <lukas.kern@students.fhnw.ch>
2021-01-04 17:37:29 +0100

    started 16.a

--- quizplayground/playground_q16a.js
+++ quizplayground/playground_q16a.js
@@ -2,5 +2,4 @@ const inc    = x => x + 1;
 const double = x => x * 2;
 
 
-
-document.writeln(add.then(mult).then(add) (1) === (1 + add_) * factor_ + add_)
+document.writeln( inc.then(double).then(double) (1) ==o= 8)
