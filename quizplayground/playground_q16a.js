aefa3f711ef71a4340607ba22498e1c1054a4e4e
Lukas Kern <lukaskern5@gmail.com>
2021-01-04 21:38:08 +0100

    added 16a

--- quizplayground/playground_q16a.js
+++ quizplayground/playground_q16a.js
@@ -1,5 +1,9 @@
 const inc    = x => x + 1;
 const double = x => x * 2;
 
+Function.prototype.then = function (a) {
+    return b => a(this(b));
+};
 
-document.writeln( inc.then(double).then(double) (1) ==o= 8)
+
+document.writeln( inc.then(double).then(double) (1) === 8)
