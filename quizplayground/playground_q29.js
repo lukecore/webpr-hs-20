853e2320887004d7040d1800531e72020b15dd9f
Lukas Kern <lukaskern5@gmail.com>
2021-01-11 21:58:28 +0100

    q29 missing

--- quizplayground/playground_q29.js
+++ quizplayground/playground_q29.js
@@ -6,14 +6,28 @@ const Observable = value => {
         setValue: val => {
             if (value === val) return;
             value = val;
-            listeners.forEach(callback => callback(val));
+            listeners.forEach(listener => listener(val));
         }
     }
 };
 
-const color = Observable('white');
-let updateCount = 0;
-const updateListener = val => updateCount++;
-color.setValue('black');
+// const color = Observable('white');
+// color.value = 'black';
+//
+// document.writeln(color.getValue() === 'black');
 
-document.writeln(updateCount === 1);
+// const color = Observable('white');
+// let updateCount = 0;
+// const updateListener = val => updateCount++;
+// color.setValue('black');
+//
+// document.writeln(updateCount === 1);
+
+// const color = Observable('white');
+// color.setValue('black');
+//
+// document.writeln(color.getValue() === 'black');
+
+const color = Observable('white');
+
+document.writeln(color.getValue() === 'white');
